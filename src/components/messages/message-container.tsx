84080f7c7cebaf8d3148f3b9fe58a1f65cc46f1c
Sichi46 <willsonmulenga7@gmail.com>
2024-09-09 16:11:32 +0200

    changes

--- src/components/messages/message-container.tsx
+++ src/components/messages/message-container.tsx
@@ -1,19 +1,25 @@
 import MessageInput from "./message-input";
 import Messages from "./messages";
+import NoChatSelected from "./no-chat -selected";
 
 const MessageContainer = () => {
+  const noChatSelected = true;
   return (
     <div className=" md:min-w-[450px] flex flex-col">
-      <>
-        {/* Header */}
-        <div className=" bg-slate-500 px-4 py-2 mb-2">
-          <span className=" label-text">To:</span>
-          <span className=" text-gray-500 font-bold">John Doe</span>
-        </div>
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          {/* Header */}
+          <div className=" bg-slate-500 px-4 py-2 mb-2">
+            <span className=" label-text">To:</span>
+            <span className=" text-gray-500 font-bold">John Doe</span>
+          </div>
 
-        <Messages />
-        <MessageInput />
-      </>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 };
