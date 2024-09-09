988a32a2549ee813f82edb9668713a81bd7892b3
Sichi46 <willsonmulenga7@gmail.com>
2024-09-09 12:52:22 +0200

    changes

--- src/components/home/side-bar.tsx
+++ src/components/home/side-bar.tsx
@@ -1,12 +1,14 @@
 import Conversations from "../sidebar/conversations";
+import LogoutButton from "../sidebar/logout-button";
 import SearchInput from "../sidebar/search-input";
 
 const SideBar = () => {
   return (
-    <div>
+    <div className=" border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
+      <LogoutButton />
     </div>
   );
 };
---
+++ src/components/sidebar/logout-button.tsx
@@ -0,0 +1,11 @@
+import { LogOut } from "lucide-react";
+
+const LogoutButton = () => {
+  return (
+    <div className=" mt-auto  ">
+      <LogOut className=" w-6 h-6 text-white cursor-pointer" />
+    </div>
+  );
+};
+
+export default LogoutButton;
