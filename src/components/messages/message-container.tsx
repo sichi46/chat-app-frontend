45c0a23d4765d0a66726be8350db01a277881906
Sichi46 <willsonmulenga7@gmail.com>
2024-09-09 13:52:33 +0200

    changes

---
+++ src/components/messages/message-container.tsx
@@ -0,0 +1,21 @@
+import MessageInput from "./message-input";
+import Messages from "./messages";
+
+const MessageContainer = () => {
+  return (
+    <div className=" md:min-w-[450px] flex flex-col">
+      <>
+        {/* Header */}
+        <div className=" bg-slate-500 px-4 py-2 mb-2">
+          <span className=" label-text">To:</span>
+          <span className=" text-gray-500 font-bold">John Doe</span>
+        </div>
+
+        <Messages />
+        <MessageInput />
+      </>
+    </div>
+  );
+};
+
+export default MessageContainer;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
 import SideBar from "../components/home/side-bar";
+import MessageContainer from "../components/messages/message-container";
 
 const Home = () => {
   return (
     <div className="flex sm:h-[550px] md:h-[550px] rounded-lg  overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <SideBar />
+      <MessageContainer />
     </div>
   );
 };
